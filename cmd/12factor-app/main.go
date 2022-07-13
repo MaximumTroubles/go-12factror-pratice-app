3cacbcc23011bd738841be0c58e13d65b3b393a0
MaximumTroubles <Rodziyevsky2@gmail.com>
2022-07-14 00:10:09 +0300

    Added Procfile for heroku

--- cmd/12factor-app/main.go
+++ cmd/12factor-app/main.go
@@ -1,7 +1,19 @@
 package main
 
-import log "github.com/sirupsen/logrus"
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
 	log.Info("Hello world")
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		log.Fatal("Port is not set")
+	}
+
+	// DB_URL heroku key if we want to use database.
+
 }
